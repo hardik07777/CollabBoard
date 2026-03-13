bc1b152d839a74fcd36d4ece4cba3f54171bf354
hardik goel <hardikgoel07@gmail.com>
2026-03-13 18:32:54 +0530

    update api base url for production

--- task-collab-frontend/src/api/axios.ts
+++ task-collab-frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/auth.store";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://collabboard-1-1q99.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
